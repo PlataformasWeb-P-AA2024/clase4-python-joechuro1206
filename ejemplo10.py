5a2d7e46f9b8f2d57fc79d383ec0afe476cefb9b
joechuro1206 <joech1206@gmail.com>
2024-04-29 23:06:40 -0500

    ejercicio10 completado

--- ejemplo10.py
+++ ejemplo10.py
@@ -10,13 +10,16 @@ lineas = archivo.readlines()
 
 lineas = [l.split("|") for l in lineas]
 
+#inicializo un contador
+contador = 1
 # Ahora imprimira la posicion 9 de las listas partidas por el split
 for x in lineas:
     cadena = """<b>Torneo:</b> %s <br> <b>Ganador: <b/>%s""" % (x[0], x[9])
     print(cadena)
-    archivo_generado = open("data/%s.html" % (x[9]), "w")
+    archivo_generado = open("data/%d_%s.html" % (contador, x[9]), "w")
     archivo_generado.writelines("%s\n" % (cadena))
     archivo_generado.close
-
+    # aumentamos el contador en cada iteracion el resultado seria 1_nombreganador hasta 25362
+    contador += 1 
     #DEBEN SALIR 25362 ARCHIVOS
    
