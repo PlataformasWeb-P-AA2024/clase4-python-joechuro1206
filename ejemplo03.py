6ee22d6d8342d367e30129b4885cf601d2394ac8
joechuro1206 <joech1206@gmail.com>
2024-04-29 19:45:24 -0500

    ejemplo 1y2

---
+++ ejemplo03.py
@@ -0,0 +1,9 @@
+nombre = input("Ingrese el nombre del persona:")
+
+edad = int(input("Ingrese edad de persona:"))
+
+sueldo = float(input("Ingrese sueldo de persona:"))
+
+mensajeFInal = "Nombre: %s\nEdad: %d\nSueldo: %.2f\n" % (nombre, edad, sueldo)
+
+print(mensajeFInal)
